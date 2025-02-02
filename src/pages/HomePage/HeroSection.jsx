df7e37c7f61ba2171e23cac7079e4ea5740bdcc0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-02 11:26:16 +0530

    Update HeroSection.jsx

--- src/pages/HomePage/HeroSection.jsx
+++ src/pages/HomePage/HeroSection.jsx
@@ -4,8 +4,8 @@ const HeroSection = () => {
   return (
     <div className='relative h-[90vh] w-auto bg-[url(https://wallpapercave.com/wp/wp4991568.jpg)] bg-cover bg-center'>
         <div className="absolute inset-0 bg-black opacity-40"></div>
-        <div className="relative xl:py-[10%] py-[20%] z-20">
-            HeroSection
+        <div className="relative xl:py-[10%] py-[20%] z-20 xl:ml-20 text-white">
+            <h1 className="xl:text-4xl text-2xl">KandyPhoto E-magazine</h1>
         </div>        
     </div>
   )
