ff6a374f04509fb839761da1eabfba9512bc1ce8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-02 11:48:29 +0530

    Create FooterData.jsx

---
+++ src/components/Footer/FooterData.jsx
@@ -0,0 +1,60 @@
+import { FaFacebook, FaInstagram,  } from "react-icons/fa";
+import { FaXTwitter, FaYoutube } from "react-icons/fa6";
+
+
+const quicklink = [
+    {
+        id: 1,
+        name: 'About',
+        link: '#',
+    },
+    {
+        id: 1,
+        name: 'Categories',
+        link: '#',
+    },
+    {
+        id: 1,
+        name: 'Articles',
+        link: '#',
+    },
+    {
+        id: 1,
+        name: 'Contact',
+        link: '#',
+    },
+    {
+        id: 1,
+        name: 'Membership',
+        link: '#',
+    },
+]
+
+const sociallinks = [
+    {
+        id: 1,
+        name: 'Facebook',
+        icon: FaFacebook,
+        link: '#'
+    },
+    {
+        id: 2,
+        name: 'Instagram',
+        icon: FaInstagram,
+        link: '#'
+    },
+    {
+        id: 3,
+        name: 'Twitter',
+        icon: FaXTwitter,
+        link: '#'
+    },
+    {
+        id: 4,
+        name: 'Youtube',
+        icon: FaYoutube,
+        link: '#'
+    },
+]
+
+export { quicklink, sociallinks }
