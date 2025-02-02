dfc726d2bc6dcc403870275f002801d76d96e483
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-02 10:06:14 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage/HomePage'
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='text-red-500 font-semibold text-xl'>App</div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<HomePage /> } />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
