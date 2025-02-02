985bf5b53744da3e98438ebdd292c7938d802aa8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-02 09:58:46 +0530

    Update

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 font-semibold text-xl'>App</div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Assuming App is your main component
+import "./index.css"; // Optional, for styling
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
