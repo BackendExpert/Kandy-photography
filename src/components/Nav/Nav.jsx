3dbe7f0d97b1095d277e2bf7793136bbdf33d3d2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-02 11:24:00 +0530

    Update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,52 +1,115 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { navmenu } from './NavManus';
 import { FaCameraRetro } from "react-icons/fa6";
+import { FaAngleDown } from "react-icons/fa";
+import { TiThMenu } from "react-icons/ti";
+import { MdOutlineClose } from "react-icons/md";
+
 
 const Nav = () => {
     const location = useLocation();
     const [isScrolled, setIsScrolled] = useState(false);
+    const [menuopen, setmenuopen] = useState(false)
+
+
+    const openmenu = () => {
+        setmenuopen(!menuopen)
+    }
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
+            setIsScrolled(window.scrollY > 50);
         };
-        
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-  return (
- <div className={`w-full py-6 transition-all duration-300 ${isScrolled ? 'bg-white text-gray-500 shadow-md' : 'bg-transparent text-white'}`}>
-        <div className="lg:mx-24 mx-8 flex justify-between">
-            <div className="">
-                <h1 className="text-xl font-semibold flex">
-                    <FaCameraRetro className='h-8 w-auto'/>
-                    <p className="pl-2">KandyPhoto  E-magazine</p>
+    return (
+        <div className={`w-full py-6 transition-all duration-300 ${isScrolled ? 'bg-white text-gray-500 shadow-md' : 'bg-transparent text-white'}`}>
+            <div className="xl:mx-24 mx-8 flex justify-between items-center">
+                {/* Logo Section */}
+                <h1 className="text-xl font-semibold flex items-center">
+                    <FaCameraRetro className="h-8 w-auto" />
+                    <p className="pl-2">KandyPhoto E-magazine</p>
                 </h1>
-            </div>
-            <div className="">
-                <div className="xl:block hidden">
-                    <div className="flex">
+
+                {/* Navigation Menu */}
+                <div className="hidden xl:block">
+                    <div className="flex space-x-6">
+                        {navmenu.map((nav, index) => (
+                            <div 
+                                key={index} 
+                                className="relative group" 
+                            >
+                                {nav.submenu.length === 0 ? (
+                                    <Link to={nav.link} className="font-semibold uppercase">
+                                        {nav.name}
+                                    </Link>
+                                ) : (
+                                    <h1 className="flex font-semibold uppercase cursor-pointer">
+                                        <p className="pr-2">{nav.name}</p>
+                                        <FaAngleDown className='h-6 w-auto'/>
+                                    </h1>
+                                )}
+
+                                {nav.submenu.length > 0 && (
+                                    <div className="absolute left-0 mt-2 w-48 bg-white text-gray-700 shadow-lg rounded-lg py-2 z-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                        {nav.submenu.map((sub, subIndex) => (
+                                            <Link 
+                                                key={subIndex} 
+                                                to={sub.link} 
+                                                className="block px-4 py-2 hover:bg-gray-200"
+                                            >
+                                                {sub.name}
+                                            </Link>
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="xl:hidden block">
+                    {
+                        !menuopen ? (
+                            <TiThMenu className='h-6 w-auto' onClick={openmenu}/>
+                        ):(
+                            <MdOutlineClose className='h-6 w-auto' onClick={openmenu}/>
+                        )
+                    }
+
+
+                    <div className={`${!menuopen ? 'hidden' : 'block'} absolute bg-white text-gray-500 py-4 pl-8 pr-16 right-8 top-20 rounded-md` }>
                         {
                             navmenu.map((nav, index) => {
                                 return (
-                                    <div className="mx-4 mt-1" key={index}>
+                                    <div className="py-4" key={index}>
                                         {
-                                            nav.submenu.length === 0 ?
-                                                (
-                                                    <Link to={nav.link}>
-                                                        <h1 className="font-semibold uppercase">{nav.name}</h1>
-                                                    </Link>
-                                                )
-                                            :
-                                                (
-                                                    <h1 className="font-semibold uppercase cursor-pointer">{nav.name}</h1>
-                                                )
+                                            nav.submenu.length === 0 ? (
+                                                <Link to={nav.link}>
+                                                    <h1 className="uppercase duration-500 hover:underline">{nav.name}</h1>
+                                                </Link>
+                                            ):(
+                                                <div className="">
+                                                    <h1 className="uppercase">{nav.name}</h1>
+
+                                                    <div className="">
+                                                        {
+                                                            nav.submenu.map((menusub, submenuindex) => {
+                                                                return (
+                                                                    <div className="pl-4 py-2" key={submenuindex}>
+                                                                        <Link to={menusub.Link}>
+                                                                            <h1 className="">{menusub.name}</h1>
+                                                                        </Link>
+                                                                    </div>
+                                                                )
+                                                            })
+                                                        }
+                                                    </div>
+                                                </div>
+                                            )
                                         }
                                     </div>
                                 )
@@ -56,8 +119,7 @@ const Nav = () => {
                 </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Nav
+export default Nav;
--- src/pages/HomePage/HeroSection.jsx
+++ src/pages/HomePage/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <div className='relative h-[90vh] w-auto bg-[url(https://wallpapercave.com/wp/wp4991568.jpg)] bg-cover bg-center'>
         <div className="absolute inset-0 bg-black opacity-40"></div>
-        <div className="relative lg:py-[10%] py-[20%] z-20">
+        <div className="relative xl:py-[10%] py-[20%] z-20">
             HeroSection
         </div>        
     </div>
